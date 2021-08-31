692a51c41fb393ea812223ecbddac2cb62e4173b
billymcd <billymcdowd@gmail.com>
2021-08-31 18:54:47 +1200

    Fixed Buttons
    Buttons now work, and print out the selected "match" to the console log

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import './App.css';
 import MatchList from "./Components/MatchList";
+import Match from "./Components/Match";
 
 function App() {
   return (
     <Router>
       <div className="container">
         <Route path="/" exact component={MatchList} />
+        <Route path="/match" exact component={Match}/>
       </div>
     </Router>
   );
