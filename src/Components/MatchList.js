0613d85a3b177588e84dd226f6176dc15dff1b6b
billymcd <billymcdowd@gmail.com>
2021-08-31 14:32:55 +1200

    Working Buttons
    Got the buttons to display correctly, with the names of the matches, and when pressed it prints the name to the sonsole.

--- src/Components/MatchList.js
+++ src/Components/MatchList.js
@@ -1,58 +1,81 @@
 import React from 'react';
 
-class Match extends React.Component {
-    constructor(props) {
-        super(props);
+// class Match extends React.Component {
+//     constructor(props) {
+//         super(props);
 
-        this.state = {
-            name: props.name,
-            age: props.age,
-            key: props.key
-        };
-    }
-}
+//         this.state = {
+//             name: props.name,
+//             age: props.age,
+//             key: props.key,
+//             press: props.press
+//         };
+//     }
+// }
 
 export default class MatchList extends React.Component {
     constructor(props) {
         super(props);
 
-        this.selectMatch = this.selectMatch.bind(this);
+        // this.selectMatch = this.selectMatch.bind(this);
 
-        this.state = { matches: [] };
+        this.state = {matches: []};
     }
 
     componentDidMount() {
-        this.state.matches.push(new Match({ name: "Bob", age: 25, key: 0 }));
-        this.state.matches.push(new Match({ name: "John", age: 30, key: 1 }));
-        this.state.matches.push(new Match({ name: "Jane", age: 28, key: 2 }));
-        this.state.matches.push(new Match({ name: "Alice", age: 22, key: 3 }));
+        var match1 = {
+            name: "Bob",
+            age: 25,
+            key: 0,
+            press: 'console.log("Bob")'
+        }
+
+        var match2 = {
+            name: "John",
+            age: 30,
+            key: 1,
+            press: 'console.log("John")'
+        }
+
+        var match3 = {
+            name: "Jane",
+            age: 28,
+            key: 2,
+            press: 'console.log("Jane")'
+        }
+
+        var match4 = {
+            name: "Alice",
+            age: 22,
+            key: 3,
+            press: 'console.log("Alice")'
+        }
+
+        this.state.matches.push(match1);
+        this.state.matches.push(match2);
+        this.state.matches.push(match3);
+        this.state.matches.push(match4);
+
+        this.matchList();
     }
 
-    selectMatch(e) {
-        //Code to display selected match here
-    }
+    // selectMatch(e) {
+    //     //Code to display selected match here
+    // }
 
     matchList() {
-        return this.state.matches.map(currentMatch => {
-            return <Match match={currentMatch} key={currentMatch.key} />;
-        })
+        this.state.matches.forEach( function(v) {
+            var button = document.createElement("button");
+            button.type = 'button';
+            button.appendChild(document.createTextNode(v.name));
+            button.setAttribute('onClick',v.press);
+            document.getElementById("buttons").appendChild(button);
+        });
     }
 
     render() {
         return (
-            <div>
-                <h3>Current Matches</h3>
-                <table className="table">
-                    <thead className="thead-light">
-                        <tr>
-                            <th>Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.matchList()}
-                    </tbody>
-                </table>
-            </div>
+            <div id="buttons"></div>
         );
     }
 }
