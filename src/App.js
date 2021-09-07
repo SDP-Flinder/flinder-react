493764adcf7f1767e8a8e5af3d9d1dadb77ed817
billymcd <billymcdowd@gmail.com>
2021-09-08 10:59:22 +1200

    Reformat
    Reformatted to attempt to look more like Daniel's forms. Attempt at making linking to the back end but getting Error 404 with the post request.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, {} from 'react';
+import React, { } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css';
+import './styles.css';
 import CreateListing from "./Components/CreateListing";
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <Route path="/" exact component={CreateListing} />
+        <Route path="/listings/add" exact component={CreateListing} />
       </div>
     </Router>
   );
