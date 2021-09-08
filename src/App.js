d2ade1b7d27d5c9022d734307808fc530dcb571d
billymcd <billymcdowd@gmail.com>
2021-09-08 13:42:38 +1200

    Minor Adjustments
    Minor formatting changes, preparing to hoot into backend.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React, {} from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route} from "react-router-dom";
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+// import { useState } from 'react';
+import './styles.css';
+import {ReactComponent as FlinderLogo} from './Components/assets/logo.svg';
 import MatchList from "./Components/MatchList";
 import Match from "./Components/Match";
 
-function App() {
-  return (
-    <Router>
-      <div className="container">
+
+//This contains all the routes to the '/signup/'
+const AppRouter = () => {
+  // //Best place to store data entered by user is here
+  // const [user, setUser] = useState({});
+
+  // const updateUser = (data) => {
+  //   setUser((prevUser) => ({ ...prevUser, ...data }));
+  // };
+
+  // const resetUser = () => {
+  //   setUser({});
+  // };
+
+  return(
+  <BrowserRouter>
+  <div className = "backround">
+    <div className = "layout">
+      <div>
+      <FlinderLogo className = "logo-display"/>
+      </div>
         <Route path="/" exact component={MatchList} />
         <Route path="/match" exact component={Match}/>
-      </div>
-    </Router>
-  );
-}
+    </div>
+  </div>
+  </BrowserRouter>
+  )
+};
 
-export default App;
+export default AppRouter;
