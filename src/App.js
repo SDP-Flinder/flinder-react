3775e914ba53d924e9bbddbfba0e4662db0a54cd
billymcd <billymcdowd@gmail.com>
2021-09-08 12:38:31 +1200

    Form Formatting
    Finished formatting the form to be in line with Daniel's styling, only outlier is the DatePicker. Post request still producing error 404

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+// import { useState } from 'react';
 import './styles.css';
+import {ReactComponent as FlinderLogo} from './Components/assets/logo.svg';
 import CreateListing from "./Components/CreateListing";
 
-function App() {
-  return (
-    <Router>
-      <div className="container">
-        <Route path="/listings/add" exact component={CreateListing} />
-      </div>
-    </Router>
-  );
-}
 
-export default App;
+//This contains all the routes to the '/signup/'
+const AppRouter = () => {
+  // //Best place to store data entered by user is here
+  // const [user, setUser] = useState({});
+
+  // const updateUser = (data) => {
+  //   setUser((prevUser) => ({ ...prevUser, ...data }));
+  // };
+
+  // const resetUser = () => {
+  //   setUser({});
+  // };
+
+  return(
+  <BrowserRouter>
+  <div className = "backround">
+    <div className = "layout">
+      <div>
+      <FlinderLogo className = "logo-display"/>
+      </div>
+        <Route path="/listings/add" exact component={CreateListing} />
+    </div>
+  </div>
+  </BrowserRouter>
+  )
+};
+
+export default AppRouter;
