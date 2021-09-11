94175fab5466dc41f38fa8107fc8bb26e0dd26a8
Finn Stainton <finnstainton@gmail.com>
2021-09-11 23:02:50 +1200

    Finished Routing on Landing, Login, Register

--- src/components/App/Authentication/index.js
+++ src/components/App/Authentication/index.js
@@ -1,82 +1,62 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
+import * as API from "../api"
 
 // Create Authentication Context
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
-// Auth Prodiver
+// Auth Provider
 export const ProvideAuth = ({ children }) => {
   const auth = useProvideAuth();
   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 };
 
-// Business Logic
 const useProvideAuth = () => {
   const [user, setUser] = useState(null);
   const isAuthed = user?.id ? true : false;
 
-  // Fetch User's Profile Data
+  // Get current user data
   useEffect(() => {
-    fetch("/users/current", {
-      headers: { "Content-type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then((res) => (res?.id ? setUser(res) : setUser(null)));
+    //TypeError: _api__WEBPACK_IMPORTED_MODULE_1__.getCurrentUser(...) is null
+    API.getCurrentUser('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MTJkNTJmN2ZmOGQ4YWM4NzJjMGRjMGEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MzEzNDUxMjEsImV4cCI6MTYzMTk0OTkyMX0.yBRm2cBo3Iv9MX1W1toJU5P5HSCh0DWHyt4Tb7nj2rg')// TODO: Find place to store token and insert into function
+    .then((res) => (
+      console.log(res)
+      (res?.id ? setUser(res) : setUser(null)
+  )));
     return () => {
       setUser(null);
     };
   }, []);
 
   // Handle signing in
-  const signin = (username, password) => {
-    return fetch("/users/authenticate", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ username, password }),
+  const signin = (username, password, remember) => {
+    API.authenticate(username, password)
+    .then(function (response) {
+      if (!response?.error && !response?.message) {
+        if (remember) { // TODO: Session management
+          return setUser(response);
+         } else {
+          return setUser(response);
+         }
+        return response;
+      }
     })
-      .then((res) => res.json())
-      .then((res) => {
-        if (!res?.error && !res?.message) {
-          return setUser(res);
-        }
-        return res;
-      })
-      .catch((error) => error);
+    .catch(function (error) {
+      // TODO: Deal with axios api errors
+    })
   };
 
-  // Handle sign out
-  const signout = () => {
-    return fetch("/users/revoke", {
-      method: "GET",
-      headers: { "Content-type": "application/json" },
-    })
-      .then(() => setUser(null))
-      .catch((error) => error);
-  };
+  // // Handle sign out
+  // const signout = () => {
+  //   return Api.revoke();
+  // };
 
-  // Handle signing up
-  const signup = (firstname, lastname, email, password) => {
-    return fetch("/users/register", {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        firstname,
-        lastname,
-        email,
-        password,
-      }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        if (!res?.error && !res?.message) {
-          return setUser(res);
-        }
-        return res;
-      })
-      .catch((error) => error);
-  };
+  // // Handle signing up
+  // const signup = (data) => {
+  //   return Api.register(data)
+  // };
 
-  return { user, isAuthed, signin, signup, signout };
+  return { user, isAuthed, signin, };
 };
 
 export const Role = {
