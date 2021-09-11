f6590aa1223f0e726941e49288f4e5475fd0b9cd
billymcd <billymcdowd@gmail.com>
2021-09-11 20:02:40 +1200

    Refactor
    Refactored all axios calls into a separate file

--- src/Components/UpdateListing.js
+++ src/Components/UpdateListing.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import DatePicker from 'react-date-picker';
 import "react-calendar/dist/Calendar.css";
 import "react-date-picker/dist/DatePicker.css";
@@ -11,7 +10,7 @@ import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import TextField from '@material-ui/core/TextField';
 import { Link as RouterLink } from 'react-router-dom';
-import { GetListings } from './AxiosHelpers';
+import { UpdateCurrentListing } from './AxiosHelpers';
 
 function UpdateListing(props) {
 
@@ -29,32 +28,6 @@ function UpdateListing(props) {
     const [utilities, setUtilities] = useState(props.listing.utilities || '');
     const [active, setActive] = useState(props.listing.active);
     const [id] = useState(props.listing.id || '');
-
-    // const loadListing = async () => {
-
-    //     setID('6139a2c2555e20c6604f113d');
-    //     const URL = 'http://localhost:4000/listings/'.concat(id);
-    //     const USER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MTM5MmNmZWE2NTE1Yzk3NzA3MjliOGEiLCJyb2xlIjoiZmxhdCIsImlhdCI6MTYzMTEzNzIxOSwiZXhwIjoxNjMxNzQyMDE5fQ.mSFRMfYIJpONB5FRRq-ED8RpkTI8zWvbF3CQDW7e-gk';
-
-    //     const config = {
-    //         headers: { Authorization: `Bearer ${USER_TOKEN}` }
-    //     };
-
-    //     axios.get(
-    //         URL,
-    //         config
-    //     ).then(response => {
-    //         setDescription(response.data.description);
-    //         setRoomAvailable(response.data.date);
-    //         setRent(response.data.rent);
-    //         setRentUnits(response.data.rentUnits);
-    //         setUtilities(response.data.utilities);
-    //     });
-    // }
-
-    // useEffect(() => {
-    //     loadListing();
-    // }, []);
 
     const onChangeDescription = (e) => {
         setDescription(e.target.value);
@@ -105,32 +78,18 @@ function UpdateListing(props) {
             console.log(isInvalid);
         } else {
 
-            const URL = 'http://localhost:4000/listings/'.concat(id);
-            const USER_TOKEN = user.token;
-
-            const config = {
-                headers: { Authorization: `Bearer ${USER_TOKEN}` }
-            };
-
-            const bodyParameters = {
+            UpdateCurrentListing({
+                user: user,
+                id: id,
                 flat_id: flat_id,
                 description: description,
                 roomAvailable: roomAvailable,
                 rent: rent,
                 rentUnits: rentUnits,
                 utilities: utilities,
-                active: active
-            };
-
-            console.log(bodyParameters);
-
-            axios.put(
-                URL,
-                bodyParameters,
-                config
-            ).then(console.log).catch(console.log);
-
-            GetListings({ user: props.user, updateListings: props.updateListings });
+                active: active,
+                updateListings: props.updateListings
+            });
 
             props.history.push('/account');
         }
