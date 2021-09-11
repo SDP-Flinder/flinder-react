9f19898a7d53d374898808cf2beeef3b5edc80b3
billymcd <billymcdowd@gmail.com>
2021-09-11 20:46:42 +1200

    Account Type Checks
    Added checks so that only an account with a role of flat can access/alter any listing data

--- src/Components/AxiosHelpers.js
+++ src/Components/AxiosHelpers.js
@@ -9,7 +9,7 @@ export async function GetUser(props) {
     await axios.post('http://localhost:4000/users/authenticate', account)
         .then(res => {
             props.updateUser(res.data);
-            GetListings({user: res.data, updateListings: props.updateListings});
+            GetListings({ user: res.data, updateListings: props.updateListings });
         })
 }
 
@@ -28,105 +28,115 @@ export async function GetListing(props) {
 }
 
 export async function GetListings(props) {
-    const URL = 'http://localhost:4000/listings/flat/'.concat(props.user.id);
-    const USER_TOKEN = props.user.token;
+    if (props.user.role === 'flat') {
+        const URL = 'http://localhost:4000/listings/flat/'.concat(props.user.id);
+        const USER_TOKEN = props.user.token;
 
-    const config = {
-        headers: { Authorization: `Bearer ${USER_TOKEN}` }
-    };
+        const config = {
+            headers: { Authorization: `Bearer ${USER_TOKEN}` }
+        };
 
-    console.log(props.user.id);
+        console.log(props.user.id);
 
-    await axios.get(URL, config)
-        .then(res => props.updateListings(res.data));
+        await axios.get(URL, config)
+            .then(res => props.updateListings(res.data));
+    }
 }
 
 export async function DeleteListing(props) {
-    const URL = 'http://localhost:4000/listings/'.concat(props.id);
-    const USER_TOKEN = props.user.token;
+    if (props.user.role === 'flat') {
+        const URL = 'http://localhost:4000/listings/'.concat(props.id);
+        const USER_TOKEN = props.user.token;
 
-    const config = {
-        headers: { Authorization: `Bearer ${USER_TOKEN}` }
-    };
+        const config = {
+            headers: { Authorization: `Bearer ${USER_TOKEN}` }
+        };
 
-    await axios.delete(URL, config)
-        .then(res => {
-            console.log(res);
-        }).then(GetListings({ user: props.user, updateListings: props.updateListings }))
+        await axios.delete(URL, config)
+            .then(res => {
+                console.log(res);
+            }).then(GetListings({ user: props.user, updateListings: props.updateListings }))
+    }
 }
 
 export async function UpdateActive(props) {
-    const URL = 'http://localhost:4000/listings/'.concat(props.listing.id);
-    const USER_TOKEN = props.user.token;
+    if (props.user.role === 'flat') {
+        const URL = 'http://localhost:4000/listings/'.concat(props.listing.id);
+        const USER_TOKEN = props.user.token;
 
-    const config = {
-        headers: { Authorization: `Bearer ${USER_TOKEN}` }
-    };
+        const config = {
+            headers: { Authorization: `Bearer ${USER_TOKEN}` }
+        };
 
-    axios.put(
-        URL,
-        { active: props.active },
-        config
-    ).then(console.log).catch(console.log);
+        axios.put(
+            URL,
+            { active: props.active },
+            config
+        ).then(console.log).catch(console.log);
 
-    GetListings({ user: props.user, updateListings: props.updateListings });
+        GetListings({ user: props.user, updateListings: props.updateListings });
+    }
 }
 
 export async function UpdateCurrentListing(props) {
-    const URL = 'http://localhost:4000/listings/'.concat(props.id);
-    const USER_TOKEN = props.user.token;
+    if (props.user.role === 'flat') {
+        const URL = 'http://localhost:4000/listings/'.concat(props.id);
+        const USER_TOKEN = props.user.token;
 
-    const config = {
-        headers: { Authorization: `Bearer ${USER_TOKEN}` }
-    };
+        const config = {
+            headers: { Authorization: `Bearer ${USER_TOKEN}` }
+        };
 
-    const bodyParameters = {
-        description: props.description,
-        roomAvailable: props.roomAvailable,
-        rent: props.rent,
-        rentUnits: props.rentUnits,
-        utilities: props.utilities,
-        active: props.active
-    };
+        const bodyParameters = {
+            description: props.description,
+            roomAvailable: props.roomAvailable,
+            rent: props.rent,
+            rentUnits: props.rentUnits,
+            utilities: props.utilities,
+            active: props.active
+        };
 
-    console.log(bodyParameters);
+        console.log(bodyParameters);
 
-    axios.put(
-        URL,
-        bodyParameters,
-        config
-    ).then(console.log).catch(console.log);
+        axios.put(
+            URL,
+            bodyParameters,
+            config
+        ).then(console.log).catch(console.log);
 
-    GetListings({ user: props.user, updateListings: props.updateListings });
+        GetListings({ user: props.user, updateListings: props.updateListings });
+    }
 }
 
 export async function CreateNewListing(props) {
-    const URL = 'http://localhost:4000/listings/add/'
-    const USER_TOKEN = props.user.token;
+    if (props.user.role === 'flat') {
+        const URL = 'http://localhost:4000/listings/add/'
+        const USER_TOKEN = props.user.token;
 
-    console.log(USER_TOKEN);
+        console.log(USER_TOKEN);
 
-    const config = {
-        headers: { Authorization: `Bearer ${USER_TOKEN}` }
-    };
+        const config = {
+            headers: { Authorization: `Bearer ${USER_TOKEN}` }
+        };
 
-    const bodyParameters = {
-        flat_id: props.user.id,
-        description: props.description,
-        roomAvailable: props.roomAvailable,
-        rent: props.rent,
-        rentUnits: props.rentUnits,
-        utilities: props.utilities,
-        active: true
-    };
+        const bodyParameters = {
+            flat_id: props.user.id,
+            description: props.description,
+            roomAvailable: props.roomAvailable,
+            rent: props.rent,
+            rentUnits: props.rentUnits,
+            utilities: props.utilities,
+            active: true
+        };
 
-    axios.post(
-        URL,
-        bodyParameters,
-        config
-    ).then(res => {
-        props.updateListing(res.data)
-    }).then(console.log).catch(console.log);
+        axios.post(
+            URL,
+            bodyParameters,
+            config
+        ).then(res => {
+            props.updateListing(res.data)
+        }).then(console.log).catch(console.log);
 
-    GetListings({ user: props.user, updateListings: props.updateListings });
+        GetListings({ user: props.user, updateListings: props.updateListings });
+    }
 }
