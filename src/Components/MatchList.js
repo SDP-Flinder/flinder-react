1c4d2c1437e71525707cce74fca355976149c76d
billymcd <billymcdowd@gmail.com>
2021-09-12 12:55:59 +1200

    Styling
    Changed the generation and styling of match buttons to align with the code used in my listing list feature.

--- src/Components/MatchList.js
+++ src/Components/MatchList.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import Match from './Match';
-// import TOKEN from '../token';
+import React, { useState } from 'react';
+import { Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 function MatchList() {
-    var match1 = {name: 'Bob', age: 25, key: 0};
-    var match2 = {name: 'John', age: 30, key: 1};
-    var match3 = {name: 'Jane', age: 28, key: 2};
-    var match4 = {name: 'Alice', age: 22, key: 3};
+    var match1 = { name: 'Bob', age: 25, key: 0 };
+    var match2 = { name: 'John', age: 30, key: 1 };
+    var match3 = { name: 'Jane', age: 28, key: 2 };
+    var match4 = { name: 'Alice', age: 22, key: 3 };
     const [matches] = useState([match1, match2, match3, match4]);
-    const [currentMatch, setCurrentMatch] = useState({name: '', age: 0, key: 0});
+    const [currentMatch, setCurrentMatch] = useState({ name: '', age: 0, key: 0 });
 
     // const getMatches = async () => {
     //     matchList();
@@ -21,40 +20,43 @@ function MatchList() {
     //         .then(res => console.log(res.data));
     // }
 
-    useEffect(() => matchList(), []);
+    const renderButtons = () => {
+        return matches.map((match) => (
+            <Button
+                className="button"
+                variant="contained"
+                key={match.key}
+                onClick={function () { selectMatch(match.key) }}
+            >
+                {match.name}
+            </Button>
+        ))
+    }
 
-    const matchList = () => {
-        // var matches = matches.slice();
-        // var currentMatch = currentMatch.slice();
-
+    const selectMatch = (key) => {
         matches.forEach(match => {
-            var button = document.createElement("button");
-            button.type = 'button';
-            button.appendChild(document.createTextNode(match.name));
-            button.onclick = function () { selectMatch(match.key) };
-            document.getElementById("buttons").appendChild(button);
-        });
-
-        function selectMatch(key) {
-            matches.forEach(match => {
-                if (match.key === key) {
-                    setCurrentMatch(match);
-                    // console.log(currentMatch);
-                    return;
-                }
-            })
-        }
+            if (match.key === key) {
+                setCurrentMatch(match);
+                return;
+            }
+        })
     }
 
     return (
         <div>
-            <h3>Pending Matches</h3>
-            <div id="buttons"></div>
-            {/* <container> */}
-            <h3>{currentMatch.name}</h3>
-            <h3>{currentMatch.age}</h3>
-            <p>{currentMatch.key}</p>
-            {/* </container> */}
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <h3>Pending Matches</h3>
+                {renderButtons()}
+                <br />
+                <h3>{currentMatch.name}</h3>
+                <h3>{currentMatch.age}</h3>
+                <p>{currentMatch.key}</p>
+            </Grid>
         </div>
     );
 }
