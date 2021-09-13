f99afec98dc73385c477cae4f8fbe47ab692f30f
danieltran-512 <xkw1927@autuni.ac.nz>
2021-09-14 07:28:18 +1200

    Update Calendar when click Back
    Fixed the issue where the calendar makes the user chose again when they click back to view their information

---
+++ src/components/Flat.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useForm, useStep } from "react-hooks-helper";
+import FlatAddress from './FlatSteps/FlatAddress';
+import FlatInfo from './FlatSteps/FlatInfo'
+import FlatChecklist from './FlatSteps/FlatChecklist'
+import Address from './FlatSteps/Address';
+
+const defaultData = {
+    firstName: '',
+    lastName: '',
+    birthday: new Date(),
+    address: {
+        street: '',
+        suburb: '',
+        city: '',
+        country: '',
+    },
+    existingFlatmates: '',
+    description: '',
+}
+
+const steps = [
+    {id: "information"},
+    {id: "address"},
+    {id: "description"},
+    {id: "checklist"}
+]
+
+const Flat = (props) => {
+    const {user} = props;
+    const [formData, setForm] = useForm(defaultData);
+    const {step, navigation} = useStep({
+        steps,
+        initialStep: 0,
+    })
+    const prop = {formData, setForm, navigation};
+
+    switch(step.id){
+        case "information":
+            return <FlatInfo {...prop} user = {user} updateUser = {props.updateUser}/>
+        case "description":
+            return <FlatAddress {...prop} user = {user} updateUser = {props.updateUser}/>
+        case "checklist": 
+            return <FlatChecklist {...prop} user = {user} updateUser = {props.updateUser}/>
+        case "address":
+            return <Address {...prop} user = {user} updateUser = {props.updateUser}/>
+    }
+
+    return(
+        <h1>Flat Sign Up</h1>
+    )
+}
+
+export default Flat
+
