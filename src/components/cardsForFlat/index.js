76963066cd066943509e35215366483d0326f033
prunellaparisa <prunella.poiney@gmail.com>
2021-09-14 13:47:46 +1200

    added basic see more feature for each card

--- src/components/cardsForFlat/index.js
+++ src/components/cardsForFlat/index.js
@@ -8,6 +8,7 @@ import _isEqual from 'lodash/isEqual';
 import CloseIcon from '@material-ui/icons/Close';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 // import TinderCards
 import TinderCard from 'react-tinder-card';
 // import base url
@@ -23,11 +24,14 @@ const CardsForFlat = () => {
   // eslint-disable-next-line no-unused-vars
   const [people, setPeople] = useState([]);
   // eslint-disable-next-line no-unused-vars
+  const [readMore, setReadMore] = useState(null);
+  const [showMore, setShowMore] = useState(true);
+  // eslint-disable-next-line no-unused-vars
   const childRefs = useMemo(() => Array(people.length).fill(0).map((i) => React.createRef()));
 
   const USER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MTJkNTJmN2ZmOGQ4YWM4NzJjMGRjMGEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MzE0MDY5NjcsImV4cCI6MTYzMjAxMTc2N30.yIXQXoZ2eTIh-OAsIZwTY1DhDkwk5ozL7-V_N_WG4YM';
   const AuthString = 'Bearer '.concat(USER_TOKEN);
-  const flatUser = 'billymcdowd'; // make a default //how to retrieve current user's username? assuming flatee pov
+  const flatUser = 'billymcd'; // make a default //how to retrieve current user's username? assuming flatee pov
   let matchparam = {
     flatUsername: flatUser,
   };
@@ -58,6 +62,9 @@ const CardsForFlat = () => {
       instance.post(addFlatee, matchparam, { headers: { Authorization: AuthString } });
     }
     alreadyRemoved.push(targetName);
+
+    setShowMore(true);
+    setReadMore(null);
   };
 
   // automate swiping feature through buttons
@@ -71,6 +78,34 @@ const CardsForFlat = () => {
       // eslint-disable-next-line max-len
       alreadyRemoved.push(toBeRemoved); // Make sure the next card gets removed next time if this card do not have time to exit the screen
       childRefs[index].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  const changeText = (person) => {
+    setShowMore(!showMore);
+    if (showMore) {
+      let smoker = '';
+      if (_isEqual(person.checklist.isSmoker, true)) {
+        smoker = 'yes';
+      } else {
+        smoker = 'no';
+      }
+      const text = (
+        <h2 style={{
+          padding: 15,
+        }}
+        >
+          {`Preferred City: ${person.preferredArea.city}`}
+          <br />
+          {`Preferred Suburb: ${person.preferredArea.suburb}`}
+          <br />
+          {`Smoker: ${smoker}`}
+        </h2>
+      );
+      setReadMore(text);
+    } else {
+      const text = null;
+      setReadMore(text);
     }
   };
 
@@ -91,11 +126,20 @@ const CardsForFlat = () => {
             >
               {/* Background image */}
               <div
-                className="card"
                 style={{
-                  background: `url(${person.imgUrl}) no-repeat center center`,
+                  backgroundImage: 'url(https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&rect=37%2C29%2C4955%2C3293&q=45&auto=format&w=926&fit=clip)',
                 }}
+                className="card"
               >
+                <body className="card-content">
+                  {readMore}
+                </body>
+                <IconButton
+                  onClick={() => changeText(person)}
+                  className="see-more-button"
+                >
+                  <MoreVertIcon fontSize="large" />
+                </IconButton>
                 {/* Name */}
                 <h3>{person.username}</h3>
               </div>
--- src/components/cardsForFlatee/index.js
+++ src/components/cardsForFlatee/index.js
@@ -24,17 +24,14 @@ const CardsForFlatee = () => {
   // eslint-disable-next-line no-unused-vars
   const [people, setPeople] = useState([]);
   // eslint-disable-next-line no-unused-vars
-  const [state, setState] = useState({
-    isExtraDescriptionShown: true,
-  });
+  const [readMore, setReadMore] = useState(null);
+  const [showMore, setShowMore] = useState(true);
   // eslint-disable-next-line no-unused-vars
   const childRefs = useMemo(() => Array(people.length).fill(0).map((i) => React.createRef()));
 
   const USER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MTJkNTJmN2ZmOGQ4YWM4NzJjMGRjMGEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MzE0MDY5NjcsImV4cCI6MTYzMjAxMTc2N30.yIXQXoZ2eTIh-OAsIZwTY1DhDkwk5ozL7-V_N_WG4YM';
   const AuthString = 'Bearer '.concat(USER_TOKEN);
   const flateeUser = 'daniel'; // make a default //how to retrieve current user's username? assuming flatee pov
-  // const element = <h1>Hello, world</h1>;
-  const url = 'url(https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&rect=37%2C29%2C4955%2C3293&q=45&auto=format&w=926&fit=clip)';
   let matchparam = {
     flateeUsername: flateeUser,
   };
@@ -65,6 +62,9 @@ const CardsForFlatee = () => {
       instance.post(addFlat, matchparam, { headers: { Authorization: AuthString } });
     }
     alreadyRemoved.push(targetName);
+
+    setShowMore(true);
+    setReadMore(null);
   };
 
   // automate swiping feature through buttons
@@ -78,6 +78,28 @@ const CardsForFlatee = () => {
       // eslint-disable-next-line max-len
       alreadyRemoved.push(toBeRemoved); // Make sure the next card gets removed next time if this card do not have time to exit the screen
       childRefs[index].current.swipe(dir); // Swipe the card!
+    }
+  };
+
+  const changeText = (person) => {
+    setShowMore(!showMore);
+    if (showMore) {
+      const text = (
+        <h2 style={{
+          padding: 15,
+        }}
+        >
+          {`Description: ${person.description}`}
+          <br />
+          {`Location: ${person.address.suburb}`}
+          <br />
+          {`${person.existingFlatmates} Flatmate(s)`}
+        </h2>
+      );
+      setReadMore(text);
+    } else {
+      const text = null;
+      setReadMore(text);
     }
   };
 
@@ -98,18 +120,20 @@ const CardsForFlatee = () => {
             >
               {/* Background image */}
               <div
-                style={{ backgroundImage: url }}
+                style={{
+                  backgroundImage: 'url(https://www.indiewire.com/wp-content/uploads/2019/10/Parasite_Parks_Garden-1.jpg?resize=1536,830)',
+                }}
                 className="card"
               >
-                <div className="card-content">
-                  <h1>Hello~!ohhhh</h1>
-                  <IconButton
-                    // onClick={() => changePic()}
-                    className="see-more-button"
-                  >
-                    <MoreVertIcon fontSize="large" />
-                  </IconButton>
-                </div>
+                <body className="card-content">
+                  {readMore}
+                </body>
+                <IconButton
+                  onClick={() => changeText(person)}
+                  className="see-more-button"
+                >
+                  <MoreVertIcon fontSize="large" />
+                </IconButton>
                 {/* Name */}
                 <h3>{person.username}</h3>
               </div>
