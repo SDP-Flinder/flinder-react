e28a2ae93b606e95a4e37c5405b6d8a690aad47e
danieltran-512 <xkw1927@autuni.ac.nz>
2021-09-14 13:56:49 +1200

    refactored
    Refactored the codes in Flat Sign Up feature

--- src/components/FlatSteps/Address.js
+++ src/components/FlatSteps/Address.js
@@ -3,10 +3,8 @@ import PlacesAutocomplete, {
     geocodeByAddress,
     getLatLng,
 } from 'react-places-autocomplete';
-//import { Button } from '@material-ui/core';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useEffect } from 'react';
 import ClearIcon from '@material-ui/icons/Clear';
 import { IconButton } from '@material-ui/core';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
@@ -33,62 +31,16 @@ const Address = (props) => {
                   country: '',
               }
 
-              for(let k =0 ; k<results[0].address_components.length; k++){
-                if(results[0].address_components[k].types[0] == 'street_number'){
-                    currentAddress.street += results[0].address_components[k].long_name;
-                  }
-    
-                  if(results[0].address_components[k].types[0] == 'route'){
-                    currentAddress.street += ' ';
-                    currentAddress.street += results[0].address_components[k].long_name;
-                  }
-    
-                  if(results[0].address_components[k].types[1] == 'sublocality'){
-                    currentAddress.suburb = results[0].address_components[k].long_name;
-                  }
-    
-                  if(results[0].address_components[k].types[0] == 'locality'){
-                    currentAddress.city = results[0].address_components[k].long_name;
-                  }
-    
-                  if(results[0].address_components[k].types[0] == 'country'){
-                    currentAddress.country = results[0].address_components[k].long_name;
-                  }
-              }
+              assignComponentAddress(results, currentAddress);
               console.log(currentAddress);
               setComponent(currentAddress);
             })
           .catch(error => console.error('Error', error));
     };
 
-
+    //Find any errors from the user iputs
     const [error,setError] = useState({});
-//    const [isInvalid, setInvalid] = useState({});
-
-    const findError = () => {
-        const errorFound = {};
-
-        if(componentAddress.country != 'New Zealand'){
-            errorFound.country = 'This app is only currently available for New Zealand adresses.';
-        }
-
-        if(!componentAddress.street){
-            errorFound.street = 'Please enter a specific street address.';
-        }
-
-        for(let k =0; k<repo.length; k++){
-            if(repo[k].role == 'flat'){
-              console.log(repo[k]);
-              if(repo[k].address.street == componentAddress.street 
-                && repo[k].address.suburb == componentAddress.suburb
-                && repo[k].address.city == componentAddress.city){
-                  errorFound.street = "This address has already been used."
-                }
-            }
-          }
-
-        return errorFound
-    }
+    const findError = checkError()
 
     const onSubmit = e => {
         e.preventDefault();
@@ -111,52 +63,19 @@ const Address = (props) => {
         }
     }
 
+    //Clear the input when the user presses X
     const clearAddress = () =>{
         setAddress('');
         setComponent({});
         setError({});
     }
 
-          //This useState is used to store data from the API
+  //This useState is used to store data from the API
   const [repo, setRepo] = useState([]);
-
   //Fetch authorised data from the API with token
-  const getRepo = async () => {
-
-        //Retrive the token
-        let token = '';
-
-        const account = {
-            username: 'admin',
-            password: 'admin'
-        }
-        
-        await axios.post('http://localhost:4000/users/authenticate', account)
-        .then(res => {
-            token = res.data.token;
-        })
-
-    const URL = 'http://localhost:4000/users/'
-    const USER_TOKEN = token;
-    const AuthString = 'Bearer '.concat(USER_TOKEN); 
-  
-    //Using .get to retrieve data 
-    await axios.get(URL, { headers: { Authorization: AuthString } })
-    .then(response => {
-      //Display for debugging
-       console.log(response.data);
-       //Store data in a local variable
-       const myRepo = response.data;
-       //Updata the state
-       setRepo(myRepo);
-    })
-    .catch((error) => {
-      //Display error
-       console.log('error ' + error);
-    });
-    }
-    //Set the data in the local state
-    useEffect(()=>getRepo(), []);
+  const getRepo = getUserData(setRepo)
+  //Set the data in the local state
+  useEffect(()=>getRepo(), []);
 
     return (
         <div>
@@ -235,6 +154,101 @@ const Address = (props) => {
             </form>
         </div>
     )
+
+    function checkError() {
+        return () => {
+            const errorFound = {};
+
+            if (componentAddress.country != 'New Zealand') {
+                errorFound.country = 'This app is only currently available for New Zealand adresses.';
+            }
+
+            if (!componentAddress.street) {
+                errorFound.street = 'Please enter a specific street address.';
+            }
+
+            for (let k = 0; k < repo.length; k++) {
+                if (repo[k].role == 'flat') {
+                    console.log(repo[k]);
+                    if (repo[k].address.street == componentAddress.street
+                        && repo[k].address.suburb == componentAddress.suburb
+                        && repo[k].address.city == componentAddress.city) {
+                        errorFound.street = "This address has already been used.";
+                    }
+                }
+            }
+
+            return errorFound;
+        };
+    }
 }
 
 export default Address
+
+function getUserData(setRepo) {
+    return async () => {
+
+        //Retrive the token
+        let token = await getToken();
+
+        const URL = 'http://localhost:4000/users/';
+        const USER_TOKEN = token;
+        const AuthString = 'Bearer '.concat(USER_TOKEN);
+
+        //Using .get to retrieve data 
+        await axios.get(URL, { headers: { Authorization: AuthString } })
+            .then(response => {
+                //Display for debugging
+                console.log(response.data);
+                //Store data in a local variable
+                const myRepo = response.data;
+                //Updata the state
+                setRepo(myRepo);
+            })
+            .catch((error) => {
+                //Display error
+                console.log('error ' + error);
+            });
+    };
+}
+
+async function getToken() {
+    let token = '';
+
+    const account = {
+        username: 'admin',
+        password: 'admin'
+    };
+
+    await axios.post('http://localhost:4000/users/authenticate', account)
+        .then(res => {
+            token = res.data.token;
+        });
+    return token;
+}
+
+function assignComponentAddress(results, currentAddress) {
+    for (let k = 0; k < results[0].address_components.length; k++) {
+        if (results[0].address_components[k].types[0] == 'street_number') {
+            currentAddress.street += results[0].address_components[k].long_name;
+        }
+
+        if (results[0].address_components[k].types[0] == 'route') {
+            currentAddress.street += ' ';
+            currentAddress.street += results[0].address_components[k].long_name;
+        }
+
+        if (results[0].address_components[k].types[1] == 'sublocality') {
+            currentAddress.suburb = results[0].address_components[k].long_name;
+        }
+
+        if (results[0].address_components[k].types[0] == 'locality') {
+            currentAddress.city = results[0].address_components[k].long_name;
+        }
+
+        if (results[0].address_components[k].types[0] == 'country') {
+            currentAddress.country = results[0].address_components[k].long_name;
+        }
+    }
+}
+
