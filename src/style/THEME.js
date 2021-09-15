7b7896f7d98a8b089b8dfee0b92285f63d604863
danieltran-512 <xkw1927@autuni.ac.nz>
2021-09-16 10:28:07 +1200

    Layout
    Edit layout

---
+++ src/style/THEME.js
@@ -0,0 +1,9 @@
+import { createMuiTheme } from 'material-ui/styles';
+
+const THEME = createMuiTheme({
+    typography: {
+     "fontFamily": `"Raleway", "Helvetica", "Arial", sans-serif`,
+    }
+ });
+
+ export default THEME;
