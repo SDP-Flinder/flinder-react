a762dea71e313464adf77125e05d61034e6ea043
billymcd <billymcdowd@gmail.com>
2021-09-16 14:58:44 +1200

    API Call
    Added API call for populating a matchlist, needs further testing though

--- src/components/Match/MatchList.js
+++ src/components/Match/MatchList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import { Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
@@ -10,7 +10,8 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Config } from '../../config';
-import { useAuth } from '../App/Authentication'
+import { useAuth } from '../App/Authentication';
+import axios from 'axios';
 
 
 function Copyright() {
@@ -49,12 +50,7 @@ const useStyles = makeStyles((theme) => ({
 function MatchList() {
     const classes = useStyles();
     const { user, getJWT } = useAuth();
-
-    var match1 = { name: 'Bob', age: 25, key: 0 };
-    var match2 = { name: 'John', age: 30, key: 1 };
-    var match3 = { name: 'Jane', age: 28, key: 2 };
-    var match4 = { name: 'Alice', age: 22, key: 3 };
-    const [matches] = useState([match1, match2, match3, match4]);
+    const [matches, setMatches] = useState([]);
     const [currentMatch, setCurrentMatch] = useState({ name: '', age: 0, key: 0 });
 
     // const getMatches = async () => {
@@ -72,8 +68,8 @@ function MatchList() {
             <Button
                 className="button"
                 variant="contained"
-                key={match.key}
-                onClick={function () { selectMatch(match.key) }}
+                key={match.id}
+                onClick={function () { selectMatch(match.id) }}
             >
                 {match.name}
             </Button>
@@ -82,12 +78,28 @@ function MatchList() {
 
     const selectMatch = (key) => {
         matches.forEach(match => {
-            if (match.key === key) {
+            if (match.id === key) {
                 setCurrentMatch(match);
                 return;
             }
         })
     }
+
+    useEffect(() => {
+        async function getMatches() {
+            const URL = 'http://localhost:4000/potentialmatchesForListing/user?flatUsername='.concat(user.username);
+            const USER_TOKEN = getJWT();
+
+            const config = {
+                headers: { Authorization: `Bearer ${USER_TOKEN}` }
+            };
+
+            const matches = await axios.get(URL, config);
+
+            setMatches(matches.data);
+        }
+            getMatches();
+    }, [user, getJWT])
 
     return (
         <Container component="main" maxWidth="xs">
