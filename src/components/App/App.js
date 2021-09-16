f8a6e0410c954ce93ef6007d654a60c7a57f9ebd
danieltran-512 <xkw1927@autuni.ac.nz>
2021-09-17 08:37:44 +1200

    Font
    Import new font

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,31 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ProvideAuth } from "./Authentication";
 import Router from "./Router";
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createTheme({
+  //This is a global font style for all components
+  typography: {
+    fontFamily: [
+      'Raleway',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  }
+});
 
 const App = () => {
   return (
+    <ThemeProvider theme={theme}>
     <ProvideAuth>
       <BrowserRouter basename="/">
         <Router />
       </BrowserRouter>
     </ProvideAuth>
+    </ThemeProvider>
   );
 };
 
