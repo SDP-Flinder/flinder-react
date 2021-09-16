bc73b9e2cbc909594b08046765a1676bf11bfa4d
prunellaparisa <prunella.poiney@gmail.com>
2021-09-16 19:52:35 +1200

    added match component and utils

---
+++ src/utils/requests.js
@@ -0,0 +1,15 @@
+// import axios
+import axios from 'axios';
+
+// const instance
+export const instance = axios.create({
+  baseURL: 'http://localhost:4000',
+});
+
+// create the base url
+export const flatsForFlatee = '/matches/getPotentialFlatAccountsForFlatee';
+export const matchesForFlatee = '/matches/potentialMatchesForFlatee';
+export const matchesForListing = '/matches/potentialMatchesForListing';
+export const addListing = '/matches/addListing';
+export const addFlatee = '/matches/addFlatee';
+export const unmatch = '/matches/unmatch';
