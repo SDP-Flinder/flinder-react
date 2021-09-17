1da77bfa6f09ed18f3a1240c29006246eba7765a
danieltran-512 <xkw1927@autuni.ac.nz>
2021-09-17 20:21:37 +1200

    Add global font
    Add a global font into the app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,28 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ProvideAuth } from "./Authentication";
 import Router from "./Router";
+import { ThemeProvider} from '@material-ui/styles';
+import { createTheme } from '@material-ui/core/styles';
+
+
+const THEME = createTheme({
+  typography: {
+   "fontFamily": `"Raleway", "Helvetica", "Arial", sans-serif`,
+   "fontWeightLight": 300,
+   "fontWeightRegular": 400,
+   "fontWeightMedium": 500
+  }
+});
 
 const App = () => {
   return (
+    <ThemeProvider theme={THEME}>
     <ProvideAuth>
       <BrowserRouter basename="/">
         <Router />
       </BrowserRouter>
     </ProvideAuth>
+    </ThemeProvider>
   );
 };
 
