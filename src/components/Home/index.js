3f0aa075dda6219f2b4acaa72a0b2aaa533f7069
danieltran-512 <xkw1927@autuni.ac.nz>
2021-09-17 20:51:53 +1200

    Create a bottom navigation
    Bottom navigation for the app

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,64 +3,62 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from "@material-ui/core";
 import { useAuth } from "../App/Authentication";
 import Navigation from "../App/Navigation";
-import CardsForFlatee from "../Match/cardsForFlatee/index";
-import CardsForFlat from "../Match/cardsForListing/index";
 import Button from '@material-ui/core/Button';
 import { Link as RouterLink } from 'react-router-dom';
+import Test from "../App/Navigation/Test";
+import { Box } from "@mui/system";
+import { CssBaseline } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  button: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    button: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }))
 
 export default function Home() {
-  const classes = useStyles();
+    const classes = useStyles();
+    const { user } = useAuth();
 
-  //Retrieve the user information from the authentication
-  const { user } = useAuth();
-
-  console.log(user);
-
-  const renderFlatButtons = () => {
-    if (user.role === 'flat') {
-      return (
-        <div>
-          <Button
-            className="button"
-            variant="contained"
-            color="primary"
-            component={RouterLink}
-            to="/listings"
-          >
-            Listings
-          </Button>
-        </div>
-      )
+    const renderFlatButtons = () => {
+        if(user.role === 'flat') {
+            return (
+                <div>
+                    <Button
+                        className="button"
+                        variant="contained" 
+                        color="primary"
+                        component={RouterLink}
+                        to="/listings"
+                    >
+                        Listings
+                    </Button>
+                </div>
+            )
+        }
     }
-  }
 
-  return (
-    <>
-      <Navigation />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-          Home
-        </Typography>
-        {/* User should only be able to access this page when authorised, but just incase. Could remove check */}
+    return (
+        <>
+            <Box sx={{ pb: 7 }}>
+            <CssBaseline />
+            <Navigation />
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                    {`Hello ${user.firstName} ${user.lastName}`}
+                </Typography>
+                {/* User should only be able to access this page when authorised, but just incase. Could remove check */}
+                {renderFlatButtons()}
+            </div>
 
-        {user.role == "flatee" ?
-          //  If the user is a flatee, render this card
-          <CardsForFlatee token={user.token} username={user.username} /> :
-          // If the user is a flat, render this card. listingID = {?}
-          renderFlatButtons()}
-      </div>
-    </>
-  );
+            <Test />
+
+            </Box>
+        </>
+    );
 };
