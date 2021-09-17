19ad7f3b59382893c541c05e4a310e364f624a40
billymcd <billymcdowd@gmail.com>
2021-09-17 20:48:00 +1200

    Fixed bug from merge
    Messed up a couple of conflict resolution during the merge, these are now fixed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,62 +9,58 @@ import Button from '@material-ui/core/Button';
 import { Link as RouterLink } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    button: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  button: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }))
 
 export default function Home() {
-    const classes = useStyles();
-    const { user } = useAuth();
+  const classes = useStyles();
 
+  //Retrieve the user information from the authentication
+  const { user } = useAuth();
 
-    //Retrieve the user information from the authentication
-    const {user} = useAuth();
+  console.log(user);
 
-    console.log(user);
-
-    const renderFlatButtons = () => {
-        if(user.role === 'flat') {
-            return (
-                <div>
-                    <Button
-                        className="button"
-                        variant="contained" 
-                        color="primary"
-                        component={RouterLink}
-                        to="/listings"
-                    >
-                        Listings
-                    </Button>
-                </div>
-            )
-        }
+  const renderFlatButtons = () => {
+    if (user.role === 'flat') {
+      return (
+        <div>
+          <Button
+            className="button"
+            variant="contained"
+            color="primary"
+            component={RouterLink}
+            to="/listings"
+          >
+            Listings
+          </Button>
+        </div>
+      )
     }
+  }
 
-    return(
-        <>
-            <Navigation />
-            <div className={classes.paper}>
-                <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-                    Home
-                </Typography>
-                {/* User should only be able to access this page when authorised, but just incase. Could remove check */}
-               
-                {user.role == "flatee"?
-                //  If the user is a flatee, render this card
-                 <CardsForFlatee token = {user.token} username = {user.username} />:
-                // If the user is a flat, render this card. listingID = {?}
-                {renderFlatButtons()}
+  return (
+    <>
+      <Navigation />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+          Home
+        </Typography>
+        {/* User should only be able to access this page when authorised, but just incase. Could remove check */}
 
-
-            </div>
-        </>
-    );
+        {user.role == "flatee" ?
+          //  If the user is a flatee, render this card
+          <CardsForFlatee token={user.token} username={user.username} /> :
+          // If the user is a flat, render this card. listingID = {?}
+          renderFlatButtons()}
+      </div>
+    </>
+  );
 };
