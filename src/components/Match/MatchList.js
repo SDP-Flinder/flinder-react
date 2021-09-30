ed4fa0c5efed045f15294cd6b8b1114de624f163
billymcd <billymcdowd@gmail.com>
2021-09-30 13:28:31 +1300

    Working list
    Match list populates. For a flat account it will display a button with the flatee username on it, however I am having difficulty getting the flat username to render on the flatee list buttons.

--- src/components/Match/MatchList.js
+++ src/components/Match/MatchList.js
@@ -1,164 +1,211 @@
 import React, { useEffect, useState } from 'react';
 // import { Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Config } from '../../config';
-import { useAuth } from '../App/Authentication';
+import { Role, useAuth } from '../App/Authentication';
 import axios from 'axios';
 
 
 function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href={`${Config.AppURL}`}>
-                {`${Config.AppName}`}
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href={`${Config.AppURL}`}>
+        {`${Config.AppName}`}
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 function MatchList(props) {
-    const classes = useStyles();
-    const { user, getJWT } = useAuth();
-    const [matches, setMatches] = useState([]);
-    const [currentMatch, setCurrentMatch] = useState({ name: '', age: 0, key: 0 });
+  const classes = useStyles();
+  const { user, jwt } = useAuth();
+  const [matches, setMatches] = useState([]);
+  const [currentMatch, setCurrentMatch] = useState({ name: '', age: 0, key: 0 });
 
-    // const listingID = '6141abba9a0320596c17bc57'; // RETRIEVE LISTING ID
-    // let matchparam = {
-    //     listingID: listingID,
-    // };
+  // const listingID = '6141abba9a0320596c17bc57'; // RETRIEVE LISTING ID
+  // let matchparam = {
+  //     listingID: listingID,
+  // };
 
-    // const getMatches = async () => {
-    //     matchList();
+  // const getMatches = async () => {
+  //     matchList();
 
-    //     const URL = 'http://localhost:4000/matches/getSuccessMatches'
-    //     const USER_TOKEN = TOKEN;
+  //     const URL = 'http://localhost:4000/matches/getSuccessMatches'
+  //     const USER_TOKEN = TOKEN;
 
-    //     axios.get(URL, { headers: { Authorization: `Bearer ${USER_TOKEN}` } })
-    //         .then(res => console.log(res.data));
-    // }
+  //     axios.get(URL, { headers: { Authorization: `Bearer ${USER_TOKEN}` } })
+  //         .then(res => console.log(res.data));
+  // }
 
-    const renderButtons = () => {
-        let count = 0;
-        return matches.map((match) => (
-            <Button
-                className="button"
-                variant="contained"
-                key={++count}
-                onClick={function () { selectMatch(match.id) }}
-            >
-                {match.username}
-            </Button>
-        ))
+  const renderButtons = () => {
+    let count = 0;
+    if (user.role === 'flat') {
+      return matches.map((match) => (
+        <Button
+          className="button"
+          variant="contained"
+          key={++count}
+          onClick={function () { selectMatch(match.id) }}
+        >
+          {match.flateeUsername}
+        </Button>
+      ))
+    }
+    else if (user.role === 'flatee') {
+      return matches.map((match) => (
+        <Button
+          className="button"
+          variant="contained"
+          key={++count}
+          onClick={function () { selectMatch(match.id) }}
+        >
+          {/* {getFlatUsername(match)} */}
+          {match.listingID}
+        </Button>
+      ))
+    }
+  }
+
+  const getFlatUsername = async(match) => {
+    var URL = 'http://localhost:4000/listings/'.concat(match.listingID);
+
+      const config = {
+        headers: { Authorization: `Bearer ${jwt}` }
+      };
+
+      const listing = await axios.get(URL, config);
+
+      URL = 'http://localhost:4000/users/'.concat(listing.data.flat_id);
+
+      const matchedUser = await axios.get(URL, config);
+
+      return matchedUser.data.username;
+  }
+
+  const selectMatch = (id) => {
+    matches.forEach(match => {
+      if (match.id === id) {
+        props.history.push({
+          pathname: '/match',
+          state: { match: match },
+        });
+        return;
+      }
+    })
+    console.log(id);
+  }
+
+  useEffect(() => {
+    async function getListings() {
+      const URL = 'http://localhost:4000/listings/flat/'.concat(user.id);
+
+      const config = {
+        headers: { Authorization: `Bearer ${jwt}` }
+      };
+
+      const listings = await axios.get(URL, config);
+
+      const listingList = listings.data;
+      console.log(listingList);
+
+      listingList.forEach(listing => {
+        getListingMatches(listing);
+      })
+      console.log(matches);
     }
 
-    const selectMatch = (id) => {
-        matches.forEach(match => {
-            if (match.id === id) {
-                props.history.push({
-                    pathname: '/match',
-                    state: { match: match },
-                });
-                return;
-            }
-        })
-        console.log(id);
+    async function getListingMatches(listing) {
+      const URL = 'http://localhost:4000/matches/getSuccessMatchesForListing';
+
+      console.log(listing.id)
+
+      const config = {
+        params: { listingID: listing.id },
+        headers: { Authorization: `Bearer ${jwt}` }
+      };
+
+      const tempMatches = await axios.get(URL, config);
+
+      console.log(tempMatches.data);
+
+      tempMatches.data.forEach(match => {
+        setMatches(matches => [...matches, match])
+      })
     }
 
-    useEffect(() => {
-        async function getListings() {
-            const URL = 'http://localhost:4000/listings/flat/'.concat(user.id);
-            const USER_TOKEN = getJWT();
+    async function getFlateeMatches() {
+      const URL = 'http://localhost:4000/matches/getSuccessMatchesForFlatee';
 
-            const config = {
-                headers: { Authorization: `Bearer ${USER_TOKEN}` }
-            };
+      const config = {
+        params: { flateeUsername: user.username },
+        headers: { Authorization: `Bearer ${jwt}` }
+      };
 
-            const listings = await axios.get(URL, config);
+      const tempMatches = await axios.get(URL, config);
 
-            const listingList = listings.data;
-            console.log(listingList);
+      console.log(tempMatches.data);
 
-            listingList.forEach(listing => {
-                getMatches(listing);
-            })
-            console.log(matches);
-        }
+      tempMatches.data.forEach(match => {
+        setMatches(matches => [...matches, match])
+      })
+    }
 
-        async function getMatches(listing) {
-            const URL = 'http://localhost:4000/matches/potentialMatchesForListing';
-            const USER_TOKEN = getJWT();
+    if (user !== null && user.role === 'flat') {
+      getListings()
+    }
+    else if (user !== null && user.role === 'flatee') {
+      getFlateeMatches()
+    }
 
-            console.log(listing.id)
+    console.log(matches);
+  }, [user, jwt])
 
-            const config = {
-                params: { listingID: listing.id },
-                headers: { Authorization: `Bearer ${USER_TOKEN}` }
-            };
 
-            const tempMatches = await axios.get(URL, config);
 
-            console.log(tempMatches.data);
-
-            tempMatches.data.forEach(match => {
-                setMatches(matches => [...matches, match])
-            })
-        }
-        if (user !== null) {
-            getListings()
-        }
-    }, [user, getJWT])
-
-    
-
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Pending Matches
-                </Typography>
-                <br />
-                {renderButtons()}
-            </div>
-            <Box mt={8}>
-                <Copyright />
-            </Box>
-        </Container>
-    );
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        {/* <Typography component="h1" variant="h5">
+          Pending Matches
+        </Typography> */}
+        <br />
+        {renderButtons()}
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
 
 export default MatchList;
