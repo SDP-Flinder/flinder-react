f6e0cd44f5b50b177cca88a9dfc711d6fc1b189e
danieltran-512 <xkw1927@autuni.ac.nz>
2021-10-06 13:24:58 +1300

    Customise theme
    Adjusted the default theme and styles

--- src/style/THEME.js
+++ src/style/THEME.js
@@ -1,9 +1,58 @@
-import { createMuiTheme } from 'material-ui/styles';
+import { createTheme } from '@material-ui/core/styles';
 
-const THEME = createMuiTheme({
+//Override themes
+const THEME = createTheme({
+
+    //Overrides primary colour
+    palette: {
+      primary: {
+        main: '#007A78',
+      },
+      secondary: {
+        main: '#FFC745',
+      }
+    },
+  
     typography: {
-     "fontFamily": `"Raleway", "Helvetica", "Arial", sans-serif`,
+     "fontFamily": `"Lexend Deca", "Helvetica", "Arial", sans-serif`,
+     "fontWeightLight": 300,
+     "fontWeightRegular": 500,
+     "fontWeightMedium": 800,
     }
- });
+  });
+  
+  THEME.overrides = {
+  
+    //Override buttons
+      MuiButton: {
+    
+        root: {
+    
+          borderRadius: 30, // round corners
+    
+          textTransform: 'none', // removes uppercase transformation
+    
+        },
+    
+        containedPrimary: {
+    
+          '&:hover': { // changes colors for hover state
+    
+            backgroundColor: THEME.palette.secondary.main,
+    
+            color: THEME.palette.primary.dark,
+    
+          },
+    
+        },
+    
+        containedSecondary: {
+    
+          fontWeight: 700, // makes text bold
+    
+        },
+    
+      },
+  }
 
  export default THEME;
