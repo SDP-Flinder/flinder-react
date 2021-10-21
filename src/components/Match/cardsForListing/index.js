32dbba45c3f347087290605d50e96cd09752dd79
danieltran-512 <xkw1927@autuni.ac.nz>
2021-10-22 11:35:43 +1300

    DISPLAY PHOTO ON THE CARDS
    The photos are visible on the cards as well

--- src/components/Match/cardsForListing/index.js
+++ src/components/Match/cardsForListing/index.js
@@ -136,7 +136,11 @@ const CardsForListing = (props) => {
               {/* Background image */}
               <div
                 style={{
-                  backgroundImage: 'url(https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&rect=37%2C29%2C4955%2C3293&q=45&auto=format&w=926&fit=clip)',
+                  backgroundImage: person.photo
+                  ? 
+                  `url(http://localhost:4000/${person.photo})`
+                  :
+                  'url(https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&rect=37%2C29%2C4955%2C3293&q=45&auto=format&w=926&fit=clip)',
                 }}
                 className="card"
               >
